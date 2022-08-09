5525377f2aabaeb03087ebadb24613cdf1e4b361
Adam Jałocha <jalochadev@gmail.com>
2022-08-09 14:58:42 +0200

    Initialized a Vue 3 app with Typescript

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  {{ user }}
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  data() {
+    return {
+      user: "test",
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.test {
+  &__2 {
+    color: red;
+  }
+}
+</style>
