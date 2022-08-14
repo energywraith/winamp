1885e1fe986a0dd22e03afdf466827a412badf88
Adam Jałocha <jalochadev@gmail.com>
2022-08-14 19:00:39 +0200

    Added shuffle, repeat checkboxes and logo to ControlButtons

--- src/components/ControlButtons.vue
+++ src/components/ControlButtons.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import IconButton from "@/components/IconButton.vue";
+import CheckboxInput from "@/components/Input/CheckboxInput.vue";
 
 import previousIcon from "@/assets/control-buttons-previous.svg";
 import playIcon from "@/assets/control-buttons-play.svg";
@@ -7,6 +9,11 @@ import pauseIcon from "@/assets/control-buttons-pause.svg";
 import stopIcon from "@/assets/control-buttons-stop.svg";
 import nextIcon from "@/assets/control-buttons-next.svg";
 import openFileIcon from "@/assets/control-buttons-open-file.svg";
+import toggleRepeatIcon from "@/assets/toggle-repeat-icon.svg";
+import logoIcon from "@/assets/logo.svg";
+
+const shuffle = ref<boolean>(false);
+const repeat = ref<boolean>(false);
 </script>
 
 <template>
@@ -16,43 +23,68 @@ import openFileIcon from "@/assets/control-buttons-open-file.svg";
       withClassicBackground
       :icon="previousIcon"
       :width="23"
-      :height="23"
+      :height="19"
     />
     <IconButton
       class="control_buttons__button"
       withClassicBackground
       :icon="playIcon"
       :width="23"
-      :height="23"
+      :height="19"
     />
     <IconButton
       class="control_buttons__button"
       withClassicBackground
       :icon="pauseIcon"
       :width="23"
-      :height="23"
+      :height="19"
     />
     <IconButton
       class="control_buttons__button"
       withClassicBackground
       :icon="stopIcon"
       :width="23"
-      :height="23"
+      :height="19"
     />
     <IconButton
       class="control_buttons__button"
       withClassicBackground
       :icon="nextIcon"
       :width="23"
-      :height="23"
+      :height="19"
     />
     <IconButton
       class="control_buttons__button control_buttons__button--open-file"
       withClassicBackground
       :icon="openFileIcon"
-      :width="21"
-      :height="23"
+      :width="22"
+      :height="17"
     />
+    <CheckboxInput
+      v-model="shuffle"
+      name="shuffle"
+      label="shuffle"
+      class="control_buttons__button control_buttons__button--shuffle"
+    />
+    <CheckboxInput
+      v-model="repeat"
+      name="repeat"
+      class="control_buttons__button control_buttons__button--repeat"
+    >
+      <img :src="toggleRepeatIcon" width="10" height="6" />
+    </CheckboxInput>
+    <a
+      href="https://github.com/energywraith/winamp"
+      target="_blank"
+      class="control_buttons__anchor"
+    >
+      <img
+        :src="logoIcon"
+        width="24"
+        height="22"
+        class="control_buttons__anchor--about"
+      />
+    </a>
   </div>
 </template>
 
@@ -60,10 +92,30 @@ import openFileIcon from "@/assets/control-buttons-open-file.svg";
 .control_buttons {
   display: flex;
   align-items: center;
+  position: relative;
 
   &__button {
     &--open-file {
       margin-left: 5px;
+    }
+
+    &--shuffle {
+      margin-left: 8px;
+    }
+
+    &--repeat {
+      margin-left: 2px;
+    }
+  }
+
+  &__anchor--about {
+    position: absolute;
+    right: -4px;
+    bottom: -4px;
+    opacity: 0.8;
+
+    &:hover {
+      opacity: 1;
     }
   }
 }
