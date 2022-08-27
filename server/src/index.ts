152e4d4ef00fd99b40325aff33459ad3e4c82c65
Adam Jałocha <jalochadev@gmail.com>
2022-08-27 14:11:57 +0200

    [server]: Eslint/Prettier + moved index.ts to src/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
-import dotenv from "dotenv"
-import path from 'path'
+import dotenv from 'dotenv';
+
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
-const view = path.join(__dirname, "../views");
+const view = path.join(__dirname, '../views');
 
 app.use(express.static(view));
 
