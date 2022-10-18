42ffa1ec9920766ce35c89c6ba8c4c7bb32e4265
Adam Jałocha <jalochadev@gmail.com>
2022-10-18 16:25:47 +0200

    [client]: Got rid of testing button

--- client/src/components/AudioPlayer.vue
+++ client/src/components/AudioPlayer.vue
@@ -8,15 +8,9 @@ const playerStore = usePlayerStore();
 const audioRef = ref<VNodeRef | null>(null);
 const audioURL = ref("");
 
-const currentLink = ref(0);
-const links = [
-  "https://www.youtube.com/watch?v=oIm-GQml3ew",
-  "https://www.youtube.com/watch?v=8Bm6eaIPNts",
-];
-
 onMounted(() => {
   playerStore.setPlayerRef(audioRef);
-  playerStore.play(links[currentLink.value]);
+  playerStore.play("https://www.youtube.com/watch?v=oIm-GQml3ew");
 
   watch(
     () => playerStore.currentSong,
@@ -49,16 +43,6 @@ onMounted(() => {
   );
 });
 
-const playOtherSong = () => {
-  if (currentLink.value === 0) {
-    currentLink.value = 1;
-  } else {
-    currentLink.value = 0;
-  }
-
-  playerStore.play(links[currentLink.value]);
-};
-
 const timeUpdate = (event: Event) =>
   playerStore.setSeeking((event.target as HTMLAudioElement).currentTime);
 </script>
@@ -67,7 +51,6 @@ const timeUpdate = (event: Event) =>
   <audio v-if="audioURL" ref="audioRef" @timeupdate="timeUpdate">
     <source :src="playerStore.currentSong?.playbackURL" />
   </audio>
-  <button @click="playOtherSong">test</button>
 </template>
 
 <style lang="scss" scoped></style>
